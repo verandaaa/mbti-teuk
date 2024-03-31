1b634ff58ccfdc2585df881a0f62a0d745da754b
verandaaa <s688922@gmail.com>
2024-04-01 06:29:31 +0900

    refactor : 글 삭제 버튼에서 작성자 정보 비교하도록 변경

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,13 +4,11 @@ import PostDeleteButton from "@/components/PostDeleteButton";
 import OptionList from "@/components/OptionList";
 import Result from "@/components/Result";
 import useGetPost from "@/hooks/useGetPost";
-import { useAuthContext } from "@/context/AuthContext";
 
 type Props = { id: string };
 
 export default function Post({ id }: Props) {
   const { data } = useGetPost(id);
-  const { user } = useAuthContext();
 
   return (
     <>
@@ -19,7 +17,7 @@ export default function Post({ id }: Props) {
           <div>
             <span>&gt; {data.categoryName}</span>
             <h1>{data.title}</h1>
-            {<PostDeleteButton isShow={data.userId === user?.userId} id={id} />}
+            {<PostDeleteButton userId={data.userId} id={id} />}
           </div>
           <div className="my-4 border-b border-black"></div>
           <div>
--- src/components/PostDeleteButton.tsx
+++ src/components/PostDeleteButton.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import { deletePost } from "@/service/postClient";
+import { useAuthContext } from "@/context/AuthContext";
 
 type Props = {
-  isShow: boolean;
+  userId: string;
   id: string;
 };
 
-export default function PostDeleteButton({ isShow, id }: Props) {
+export default function PostDeleteButton({ userId, id }: Props) {
+  const { user } = useAuthContext();
+
   const handleDeletePostButtonClick = () => {
     deletePost(id);
   };
+
+  const isShow = userId === user?.userId;
 
   return isShow && <button onClick={handleDeletePostButtonClick}>삭제</button>;
 }

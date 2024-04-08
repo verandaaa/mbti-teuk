07527976b60648ac0748e4a8e8ea994280d7f810
verandaaa <s688922@gmail.com>
2024-04-09 03:02:26 +0900

    feat : 글 삭제 후 리스트로 복귀

--- src/components/PostDeleteButton.tsx
+++ src/components/PostDeleteButton.tsx
@@ -2,6 +2,7 @@
 
 import { deletePost } from "@/service/postClient";
 import { useAuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
 
 type Props = {
   userId: string;
@@ -10,9 +11,11 @@ type Props = {
 
 export default function PostDeleteButton({ userId, id }: Props) {
   const { user } = useAuthContext();
+  const router = useRouter();
 
   const handleDeletePostButtonClick = () => {
     deletePost(id);
+    router.push(`/list`);
   };
 
   const isShow = userId === user?.userId;
